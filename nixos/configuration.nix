96f76bede77a38c516157324414e56072e739d84
Luiz Fernando <luizfernandoklein@live.com>
2024-03-10 01:36:13 -0300

    feat: Pipewire audio

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,6 +69,17 @@
     exportConfiguration = true;
   };
 
+  # Audio
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   # Hardware stuff
   hardware = {
     opengl = {
