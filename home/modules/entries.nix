f37bef732776800b7b0ff633bb310741cd84e32a
LuizFerK <luizfernandoklein@live.com>
2025-04-07 23:24:17 -0300

    chore: Vesktop to use xwayland

--- home/modules/entries.nix
+++ home/modules/entries.nix
@@ -11,5 +11,14 @@
       terminal = false;
       type = "Application";
     };
+    vesktop = {
+      categories = ["Application"];
+      exec = "vesktop -enable-features=UseOzonePlatform -ozone-platform=wayland";
+      genericName = "Vesktop";
+      icon = "vesktop";
+      name = "Vekstop";
+      terminal = false;
+      type = "Application";
+    };
   };
 }
